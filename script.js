f4ff5ff1090140029b94a5accd4ac52e144a67ac
Anghell21 <106360473+Anghell21@users.noreply.github.com>
2024-05-09 15:56:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Função para gerar uma senha com base nas opções selecionadas
+function generatePassword() {
+    const length = parseInt(document.getElementById('length').value);
+    const includeUppercase = document.getElementById('includeUppercase').checked;
+    const includeLowercase = document.getElementById('includeLowercase').checked;
+    const includeNumbers = document.getElementById('includeNumbers').checked;
+    const includeSymbols = document.getElementById('includeSymbols').checked;
+
+    let charset = '';
+    if (includeUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (includeLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (includeNumbers) charset += '0123456789';
+    if (includeSymbols) charset += '!@#$%^&*()-_=+';
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+// Event listener para o botão de gerar senha
+document.getElementById('generate-btn').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    passwordField.value = generatePassword();
+});
